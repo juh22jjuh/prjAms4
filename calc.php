e81ae975310a30bf8d6c9fa243807038057d5061
Glaubinho03 <85969179+juh22jjuh@users.noreply.github.com>
2022-10-06 11:47:32 -0300

    Add files via upload

--- calc.php
+++ calc.php
@@ -2,8 +2,6 @@
     $Numero = $_POST['txtNumero'];
     $triangulo = ($Numero - 1);
     $Pos = ($Numero + 1);
-    $re = ($Numero * $Numero);
     echo "Antecessor: " , $triangulo;
     echo "Posterior: " , $Pos;
-    echo "Quadrado: " , $re;
  ?>
